d04fd3c252756cf58909c7c0216443674abf1322
Danilopmz <danilopmzxbox@hotmail.com>
2018-04-24 11:20:43 -0300

    Estrutura e variaveis

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Logica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //   for(int i=10; i >= 1 ; i--) 
+        
+        //    Console.WriteLine(i);
+
+            string cpfCliente;
+            string cpf9;
+            string cpf10;
+            int contagem1=10;
+            int contagem2=11;
+            int resto=0
+            
+
+            /* Vamos usar um comandos substring para obter apenas uma arte do nome Helena.
+            Neste comando voce deve informar o ponto de partida e a quantidade de caracteres que devem ser capturados */
+            
+            /* >>>Console.WriteLine(nome.substring(0,3))
+            
+
+            // Console.WriteLine(nome.Length);// o comandos Length pega a quantidade de caracteres
+            // for(int i=nome.Length-1; i >= 0; i--)
+            //     Console.Write(nome[i]);
+        }
+    }
+}
